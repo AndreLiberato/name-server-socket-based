b1de685daa4cb55374c0319d38bc60d92ad841fe
Victor Xavier <victorxavier530@gmail.com>
2024-04-21 17:21:09 -0300

    Criação do stub

---
+++ name-server/target/debug/deps/name_server-8442eff5be54af78.d
@@ -0,0 +1,5 @@
+C:\Users\Victor Xavier\Documents\cod_rust\name-server-socket-based\name-server\target\debug\deps\libname_server-8442eff5be54af78.rmeta: src\main.rs
+
+C:\Users\Victor Xavier\Documents\cod_rust\name-server-socket-based\name-server\target\debug\deps\name_server-8442eff5be54af78.d: src\main.rs
+
+src\main.rs:
---
+++ stub/target/debug/deps/stub-282b4488f62a277f.d
@@ -0,0 +1,5 @@
+C:\Users\Victor Xavier\Documents\cod_rust\name-server-socket-based\stub\target\debug\deps\libstub-282b4488f62a277f.rmeta: src\main.rs
+
+C:\Users\Victor Xavier\Documents\cod_rust\name-server-socket-based\stub\target\debug\deps\stub-282b4488f62a277f.d: src\main.rs
+
+src\main.rs:
